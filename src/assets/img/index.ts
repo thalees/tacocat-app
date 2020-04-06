cea640e43ea9ffe66d417389ff145173dbc12b0e
Thales Pereira <thalespereiradejesus@gmail.com>
2020-04-05 23:47:57 -0300

    Added and fixed images

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -2,4 +2,10 @@ export const images = {
   black: require('./taco.png'),
   blue: require('./dog.png'),
   green: require('./cat.png'),
+  background: require('./background.png'),
+  backgroundTopBar: require('./top-bar.png'),
+  backgroundBottomBar: require('./bottom-bar.png'),
+  active: require('./button-active.png'),
+  pushed: require('./button-pushed.png'),
+  inactive: require('./button-inactive.png'),
 };
